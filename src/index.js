71209912b63ea20e7bcc625284381e9075508c05
Alexander Korniichuk <world.kor.films@gmail.com>
2021-10-26 10:18:29 +0300

    SnapShot Testing (src/Link)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Link from "./Link/Link"
+
+const destination = document.querySelector("#container");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <>
+        <h1>Hello</h1>
+        <Link>Link to ...</Link>
+    </>,
+    destination
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
