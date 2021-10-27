cca10e121d0dbacc126d4fddb7f9c8d7caac695d
Alexander Korniichuk <world.kor.films@gmail.com>
2021-10-27 09:41:45 +0300

    DOM Testing small ex

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Link from "./Link/Link"
+import CheckboxWithLabel from "./CheckboxWithLabel/CheckboxWithLabel";
 
 const destination = document.querySelector("#container");
 
@@ -9,6 +10,7 @@ ReactDOM.render(
     <>
         <h1>Hello</h1>
         <Link>Link to ...</Link>
+        <CheckboxWithLabel labelOn={"On"} labelOff={"Off"}/>
     </>,
     destination
 );
