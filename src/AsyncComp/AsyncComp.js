e9bb4a54a4319f367e1317b1c0c953f7d817eb5f
Alexander Korniichuk <world.kor.films@gmail.com>
2021-11-03 09:31:49 +0200

    For the question How to imitate error in fetch to display content in case error

--- src/AsyncComp/AsyncComp.js
+++ src/AsyncComp/AsyncComp.js
@@ -1,4 +1,5 @@
 import React, {useEffect, useState} from "react"
+import "./asyncComp.css"
 
 export const AsyncComp = () => {
     const [users, setUsers] = useState([])
@@ -7,13 +8,37 @@ export const AsyncComp = () => {
     useEffect(() => {
         fetch('https://61810e218bfae60017adfdcd.mockapi.io/users')
             .then(response => response.json())
-            .then(json => console.log(JSON.stringify(json, undefined, 2)))
+            .then(json => setUsers(json))
+            .catch(e => {
+                console.log("error = ", e)
+                setError(e)
+            })
     }, [])
 
+    const displayUsers = (users) => {
+        if (!Array.isArray(users)) return false
+
+        return users.map(item => {
+            return <User key={item.id} name={item.name} avatar={item.avatar}/>
+        })
+    }
 
     return (
         <>
             <p>Lorem ipsum dolor sit amet, consectetur.</p>
+            <div className={"users"}>
+                {error && <p className={"error"}>Something has just gone wrong! ({error})</p>}
+                {displayUsers(users)}
+            </div>
         </>
+    )
+}
+
+const User = ({name, avatar}) => {
+    return (
+        <div className={"user"}>
+            <img src={avatar} alt="error"/>
+            <p><b>{name}</b></p>
+        </div>
     )
 }
